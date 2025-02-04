0a4b240d40a236cbf42e8776e91f960497e4ff35
yonatanlla <yonatanllanto174@gmail.com>
2025-02-04 14:29:57 -0500

    four commit

--- src/components/BentoGrid.vue
+++ src/components/BentoGrid.vue
@@ -1,36 +1,36 @@
-<script setup lang="ts"></script>
+<script></script>
 <template>
   <div class="">
     <div class="grid grid-cols-2">
       <div>
-        <img v-lazy="'../../public/perfil_6.jpeg'" alt="" />
-        <img v-lazy="'../../public/perfil_3.jpeg'" alt="" />
+        <img src="../../public/perfil_6.jpeg" alt="" />
+        <img src="../../public/perfil_3.jpeg" alt="" />
       </div>
       <div class="">
         <img
           class="w-full h-full object-cover"
-          v-lazy="'../../public/perfil_5.jpeg'"
+          src="../../public/perfil_5.jpeg"
           alt=""
         />
       </div>
       <img
         class="w-full h-full object-cover"
-        v-lazy="'../../public/perfil_1.jpeg'"
+        src="../../public/perfil_1.jpeg"
         alt=""
       />
       <img
         class="w-full h-full object-cover"
-        v-lazy="'../../public/perfil_2.jpeg'"
+        src="../../public/perfil_2.jpeg"
         alt=""
       />
       <img
         class="w-full h-full object-cover"
-        v-lazy="'../../public/perfil_4.jpeg'"
+        src="../../public/perfil_4.jpeg"
         alt=""
       />
       <img
         class="w-full h-full object-cover"
-        v-lazy="'../../public/perfil_9.jpg'"
+        src="../../public/perfil_9.jpg"
         alt=""
       />
     </div>
